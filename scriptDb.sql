c0ead15e61a69d8183063bdc61407719c1d7333a
Pablo <pablo.silva.edu@gmail.com>
2025-06-02 10:48:23 -0300

    Backup de arquivos antigos

--- scriptDb.sql
+++
@@ -1,9 +0,0 @@
--- Criação do banco de dados e tabela para scores
-CREATE DATABASE IF NOT EXISTS supermario;
-USE supermario;
-
-CREATE TABLE IF NOT EXISTS scores (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    score INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
